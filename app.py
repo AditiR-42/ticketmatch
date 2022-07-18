cb4d7de02fb3b39701614afb1d3484b12d698fde
AditiR_42 <aditisanram@gmail.com>
2022-07-18 17:59:43 -0400

    Change to SQLite

--- app.py
+++ app.py
@@ -24,11 +24,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use PostgreSQL database
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
-db = SQL(uri)
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///match.db")
 
 @app.after_request
 def after_request(response):
