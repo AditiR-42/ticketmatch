a5b4adb33f858bbff118ea8f4b24110ec2baa65b
AditiR_42 <aditisanram@gmail.com>
2022-07-18 23:22:13 -0400

    Update comment

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required, usd
 
 # Configure application
 app = Flask(__name__)
@@ -24,7 +24,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure SQLite database
 db = SQL("sqlite:///match.db")
 
 @app.after_request
