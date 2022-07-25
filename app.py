e7e842ff29a1fc1719fd54a3cc018c5f2fad8e8d
AditiR_42 <aditisanram@gmail.com>
2022-07-25 00:14:34 -0400

    Update configure session

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.jinja_env.filters["usd"] = usd
 
 # Configure session
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
